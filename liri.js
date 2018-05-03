bc6d611559a6fdcdb3e4bb8211509dc90ff506ce
Krista Neel <>
2018-05-03 18:01:42 -0500

    set up

---
+++ liri.js
@@ -0,0 +1,10 @@
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var request = require('request');
+
+
+
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
