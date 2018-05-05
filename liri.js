9fb0b4c7e816b8864bc75000208d8f292da4a528
Krista Neel <>
2018-05-04 22:49:55 -0500

    yikes

--- liri.js
+++ liri.js
@@ -1,10 +1,11 @@
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
+var keys = require('./keys.js');  
 
-
-
-
+var nodeArgs = process.argv; 
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+module.exports = keys;
